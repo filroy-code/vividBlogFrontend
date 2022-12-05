abd5631b861860fc79809f229e0f04dd3c694c19
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-12-05 09:11:45 -0500

    Added search page loading skeleton.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,6 +4,7 @@ import useSWR from "swr";
 import { IBlog } from "../types/blog";
 import PreviewBox from "./PreviewBox";
 import PaginationButtons from "./PaginationButtons";
+import SearchLoadingSkeleton from "./SearchLoadingSkeleton";
 
 export default function Search(): JSX.Element {
   const [pageNumber, setPageNumber] = React.useState(0);
@@ -13,12 +14,15 @@ export default function Search(): JSX.Element {
 
   return (
     <div className="searchPage">
-      <div className="blogList">
-        {data &&
-          data.blogList.map((datum: IBlog) => {
+      {data && data.blogList ? (
+        <div className="blogList">
+          {data.blogList.map((datum: IBlog) => {
             return <PreviewBox key={datum.id} blogData={datum} />;
           })}
-      </div>
+        </div>
+      ) : (
+        <SearchLoadingSkeleton />
+      )}
       <PaginationButtons setPageNumber={setPageNumber} />
     </div>
   );
---
+++ src/components/SearchLoadingSkeleton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Skeleton from "@mui/material/Skeleton";
+
+export default function SearchLoadingSkeleton() {
+  const skeletonArray = [0, 1, 2, 3, 4, 5];
+  const skeletonDisplay = skeletonArray.map((item) => {
+    return (
+      <Skeleton className="blogPreview" key={item} height={300}></Skeleton>
+    );
+  });
+  return <div className="blogList">{skeletonDisplay}</div>;
+}
